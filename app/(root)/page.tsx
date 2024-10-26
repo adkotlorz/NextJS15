21e8a3f58ddc87875b222b5756fec4ff23ab17de
adkotlorz <ad.kotlorz@gmail.com>
2024-10-26 21:04:02 +0200

    added shadcn and hero section with query in the search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
-  return (
-    <>
-    <h1 className="text-2xl">Home</h1>
-    </>
-  );
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({searchParams}: { searchParams: Promise<{ query?: string }> }) {
+	const query = (await searchParams).query;
+
+
+	return (
+		<>
+			<section className="pink_container">
+				<h1 className="heading">Pitch Your Startup, <br /> connect with entrepreneurs</h1>
+				<p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, Get noticed</p>
+				<SearchForm query={query} />
+			</section>
+		</>
+	);
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
-import {auth, signOut, signIn} from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 
 const Navbar = async () => {
-	const session = await auth()
+	const session = await auth();
 
 	return (
 		<header className="px-5 py-3 bg-white shadow-sm font-work-sans">
 			<nav className="flex items-center justify-between">
-			<Link href="/">
-				<Image
-				src="/logo.png"
-				alt="Logo"
-				width={144}
-				height={30}
-				/>
-			</Link>
+				<Link href="/public">
+					<Image
+						src="/logo.png"
+						alt="Logo"
+						width={144}
+						height={30}
+					/>
+				</Link>
 				<div className="flex items-center gap-5 text-black">
 					{session && session?.user ? (
 						<>
@@ -25,7 +25,7 @@ const Navbar = async () => {
 							<form action={async () => {
 								"use server";
 
-								await signOut({redirectTo: "/"})
+								await signOut({redirectTo: "/"});
 							}}>
 								<button type="submit">Logout</button>
 							</form>
@@ -34,21 +34,19 @@ const Navbar = async () => {
 								<span>{session?.user?.name}</span>
 							</Link>
 						</>
-
 					) : (
 						<form action={async () => {
 							"use server";
 
-							await signIn('github')
+							await signIn("github");
 						}}>
-
 							<button type="submit">
 								Login
 							</button>
 						</form>
 					)}
 				</div>
-		</nav>
+			</nav>
 		</header>
 	);
 };
