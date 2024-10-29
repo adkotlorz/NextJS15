a30dbf3e60b037eae31f9a57dfdff90d8286de5e
adkotlorz <ad.kotlorz@gmail.com>
2024-10-29 16:38:35 +0100

    startup form ready, working on useActionState hook to submit the form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import StartupCard, {
+	StartupTypeCard,
+} from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { auth } from "@/auth";
 
-export default async function Home({searchParams}: { searchParams: Promise<{ query?: string }> }) {
+export default async function Home({searchParams}: {
+	searchParams: Promise<{ query?: string }>
+}) {
 	const query = (await searchParams).query;
 	const params = {search: query || null};
 
@@ -12,13 +16,18 @@ export default async function Home({searchParams}: { searchParams: Promise<{ que
 
 	console.log(session);
 
-	const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params});
+	const {data: posts} = await sanityFetch({
+		query: STARTUPS_QUERY,
+		params,
+	});
 
 	return (
 		<>
 			<section className="pink_container">
-				<h1 className="heading">Pitch Your Startup, <br /> connect with entrepreneurs</h1>
-				<p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, Get noticed</p>
+				<h1 className="heading">Pitch Your
+					Startup, <br /> connect with entrepreneurs</h1>
+				<p className="sub-heading !max-w-3xl">Submit ideas,
+					Vote on Pitches, Get noticed</p>
 				<SearchForm query={query} />
 			</section>
 
